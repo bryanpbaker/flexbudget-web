7868d111eee44dab78bd4d738bfb3b10abd65a5e
Bryan Baker <bryanpbaker@gmail.com>
2017-09-09 14:29:02 -0500

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const port = provess.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/public', 'index.html'));
+});
+
+app.listen(port);
